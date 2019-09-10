7e74279ab1a169bf8bbc7bdf66c71377785c3910
cvsgbhn <54961302+cvsgbhn@users.noreply.github.com>
2019-09-10 17:54:25 +0300

    Add files via upload

--- ex26/ft_count_if.c
+++ ex26/ft_count_if.c
@@ -1,13 +1,25 @@
-int	ft_count_if(char *tab, int(*f)(char *))
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   if_count_if.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vdanilo <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/08 14:59:48 by vdanilo           #+#    #+#             */
+/*   Updated: 2019/09/10 17:44:57 by vdanilo          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int		ft_count_if(char **tab, int (*f)(char *))
 {
 	int counter;
 	int iterator;
 
 	counter = 0;
 	iterator = 0;
-	while (tab[i])
+	while (tab[iterator])
 	{
-		if((*f)(tab[i] == 1);
+		if ((*f)(tab[iterator]) == 1)
 			counter++;
 		iterator++;
 	}
--- ex26/main.c
+++ ex26/main.c
@@ -2,24 +2,17 @@
 
 int     ft_count_if(char **tab, int (*f)(char *));
 
-int		print_all(char **tab)
+int		print_all(char *tab)
 {
 	int i;
-	int j;
 	
 	i = 0;
-	j = 0;
 	while (tab[i])
-	{
-		while (tab[i][j])
-		{
-			printf("%c", tab[i][j]);
-			j++;
-		}
+	{	
+		printf("%c", tab[i]);
 		i++;
-		return (1);
 	}
-	return (0);
+	return (1);
 }
 
 int		main(int argc, char **argv)
@@ -27,10 +20,11 @@ int		main(int argc, char **argv)
 	int count_if;
 
 	count_if = 0;
-	int (*f)(char **);
-	f = &print_all;
+	int (*fun)(char *);
+	fun = &print_all;
 	if (argc <2)
 		return (0);
-	count_if = ft_count_if(argv, (*f)(argv));
+	count_if = ft_count_if(argv, fun);
+	printf("%d", count_if);
 	return (0);
 }
