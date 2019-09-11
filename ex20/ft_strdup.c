6f309853c9ead49bdc3c9688c37a6f191f9c4159
cvsgbhn <54961302+cvsgbhn@users.noreply.github.com>
2019-09-11 16:40:38 +0300

    Add files via upload

--- ex20/ft_strdup.c
+++ ex20/ft_strdup.c
@@ -1,6 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vdanilo <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/08 15:04:30 by vdanilo           #+#    #+#             */
+/*   Updated: 2019/09/11 16:39:42 by vdanilo          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdlib.h>
 
-int	ft_strlen(char *str)
+int		ft_strlen(char *str)
 {
 	int counter;
 
@@ -13,22 +25,25 @@ int	ft_strlen(char *str)
 	return (counter);
 }
 
-char *ft_strcpy(char *string1, char *string2)
+char	*ft_strcpy(char *string1, char *string2)
 {
-	while (*string1 && *string2)
+	while (*string1)
 	{
 		*string2 = *string1;
 		string1++;
 		string2++;
 	}
+	*string2 = '\0';
 	return (string2);
 }
 
 char	*ft_strdup(char *src)
 {
 	char *copy;
+
 	copy = (char *)malloc(sizeof(char) * (ft_strlen(src) + 1));
-	if (!copy) return ("");
-	ft_strcpy(copy, src);
-	return (src);
+	if (!copy)
+		return ("");
+	ft_strcpy(src, copy);
+	return (copy);
 }
